2c11c7d5e16fc6f68286337e5212e53196e6b7ea
Veniamin Polienko <kasaklalita@mail.ru>
2022-03-20 16:17:59 +0300

    InventoryManager Class

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : SingletonMonobehaviour<InventoryManager>
+{
+    private Dictionary<int, ItemDetails> itemDetailsDectionary;
+
+    [SerializeField] private SO_ItemList itemList = null;
+
+    private void Start()
+    {
+        //Create item details dictionary
+        CreateItemDetailsDictionary();
+    }
+
+    /// <summary>
+    /// Populates the itemDetailsDictionary from the scriptable object items list
+    /// </summary>
+
+    private void CreateItemDetailsDictionary()
+    {
+        itemDetailsDectionary = new Dictionary<int, ItemDetails>();
+
+        foreach (ItemDetails itemDetails in itemList.itemDetails)
+        {
+            itemDetailsDectionary.Add(itemDetails.itemCode, itemDetails);
+        }
+    }
+
+    /// <summary>
+    /// Returns the itemDetails (from the SO_itemList) for the itemcode, or null is the item code doesnt`t exist
+    /// </summary>
+    /// <param name="itemCode"></param>
+    /// <returns></returns>
+
+    public ItemDetails GetItemDetails(int itemCode)
+    {
+        ItemDetails itemDetails;
+
+        if (itemDetailsDectionary.TryGetValue(itemCode, out itemDetails))
+        {
+            return itemDetails;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
