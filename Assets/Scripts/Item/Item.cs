7af7eabdee8689f3e6cf4e3a9dddb76832074481
Veniamin Polienko <kasaklalita@mail.ru>
2022-03-25 13:11:01 +0300

    Custom Property Attribute Drawer

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    [ItemCodeDescription]
+    [Range(10000, 10100)]
     [SerializeField]
     private int _itemCode;
     public int ItemCode { get { return _itemCode; } set { _itemCode = value; } }
