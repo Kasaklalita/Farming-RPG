143c5526a45955e068a58edd998ea1e035ae04ed
Veniamin Polienko <kasaklalita@mail.ru>
2022-03-26 13:53:29 +0300

    Drop Items From Inventory Bar

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -54,22 +54,26 @@ public class Player : SingletonMonobehaviour<Player>
     {
         #region Player Input
 
-        ResetAnimationTriggers();
-        PlayerMovementInput();
-        PlayerWalkInput();
-        EventHandler.CallMovementEvent(
-            xInput,
-            yInput,
-            isWalking,
-            isRunning,
-            isIdle,
-            isCarrying,
-            toolEffect,
-            isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
-            isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
-            isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
-            isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
-            false, false, false, false);
+        if (!PlayerInputIsDisabled)
+        {
+            ResetAnimationTriggers();
+            PlayerMovementInput();
+            PlayerWalkInput();
+            EventHandler.CallMovementEvent(
+                xInput,
+                yInput,
+                isWalking,
+                isRunning,
+                isIdle,
+                isCarrying,
+                toolEffect,
+                isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+                isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+                isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+                isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                false, false, false, false);
+
+        }
 
         #endregion
     }
@@ -170,6 +174,46 @@ public class Player : SingletonMonobehaviour<Player>
             isIdle = false;
             movementSpeed = Settings.runningSpeed;
         }
+    }
+
+    private void ResetMovement()
+    {
+        xInput = 0f;
+        yInput = 0f;
+        isRunning = false;
+        isWalking = false;
+        isIdle = true;
+    }
+
+    public void DisablePlayerInputAndResetMovement()
+    {
+        DisablePlayerInput();
+        ResetMovement();
+
+        //Send event to any listeners for player movement input
+        EventHandler.CallMovementEvent(
+                xInput,
+                yInput,
+                isWalking,
+                isRunning,
+                isIdle,
+                isCarrying,
+                toolEffect,
+                isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+                isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+                isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+                isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                false, false, false, false);
+    }
+
+    public void DisablePlayerInput()
+    {
+        PlayerInputIsDisabled = true;
+    }
+
+    public void EnablePlayerInput()
+    {
+        PlayerInputIsDisabled = false;
     }
 
     public Vector3 GetPlayerViewportPosition()
--- Assets/Scripts/UI/UIInventory/UIInventorySlot.cs
+++ Assets/Scripts/UI/UIInventory/UIInventorySlot.cs
@@ -1,14 +1,98 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UIInventorySlot : MonoBehaviour
+public class UIInventorySlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    private Camera mainCamera;
+    private Transform parentItem;
+    private GameObject draggedItem;
+
     public Image inventorySlotHighlight;
     public Image inventorySlotImage;
     //public TextMeshProUGUI textMeshProUGUI;
     public Text text;
 
+    [SerializeField] private UIInventoryBar inventoryBar = null;
     [HideInInspector] public ItemDetails itemDetails;
+    [SerializeField] private GameObject itemPrefab = null;
     [HideInInspector] public int itemQuantity;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        parentItem = GameObject.FindGameObjectWithTag(Tags.ItemsParentTransform).transform;
+
+
+    }
+
+    /// <summary>
+    /// Drops the item (if selected) at the current mouse position. Called by the DropItem event
+    /// </summary>
+    private void DropSelectedItemAtMousePosition()
+    {
+        if (itemDetails != null)
+        {
+            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -mainCamera.transform.position.z));
+
+            //Create item from prefab at mouse position
+            GameObject itemGameObject = Instantiate(itemPrefab, worldPosition, Quaternion.identity, parentItem);
+            Item item = itemGameObject.GetComponent<Item>();
+            item.ItemCode = itemDetails.itemCode;
+
+            //Remove item from player`s inventory
+            InventoryManager.Instance.RemoveItem(InventoryLocation.player, item.ItemCode);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (itemDetails != null)
+        {
+            //Disable keyboard input
+            Player.Instance.DisablePlayerInputAndResetMovement();
+
+            //Instantiate gameObject as dragged item
+            draggedItem = Instantiate(inventoryBar.inventoryBarDraggedItem, inventoryBar.transform);
+
+            //Get image for dragged item
+            Image draggedItemImage = draggedItem.GetComponentInChildren<Image>();
+            draggedItemImage.sprite = inventorySlotImage.sprite;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Move game object as dragged item
+        if (draggedItem != null)
+        {
+            //draggedItem.transform.position = Input.mousePosition;
+            draggedItem.transform.position = new Vector3(Input.mousePosition.x - 8, Input.mousePosition.y - 8, 0.0f);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Destroy game object as dragged item
+        if (draggedItem != null)
+        {
+            Destroy(draggedItem);
+
+            //If drag ends overinventory bar, get item drag is over and swap them
+            if (eventData.pointerCurrentRaycast.gameObject != null && eventData.pointerCurrentRaycast.gameObject.GetComponent<UIInventorySlot>() != null)
+            {
+
+            }
+            //Else attempt to drom the item if it can be dropped
+            else
+            {
+                if (itemDetails.canBeDropped)
+                {
+                    DropSelectedItemAtMousePosition();
+                }
+            }
+            Player.Instance.EnablePlayerInput();
+        }
+    }
 }
